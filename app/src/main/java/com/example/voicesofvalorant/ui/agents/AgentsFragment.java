ea2f69911ed4a51a781959099fce6d22da8b4f6e
Kalvin <kalvin.kao@hotmail.com>
2020-12-01 05:58:10 -0500

    update agentfrag, added fonts

--- app/src/main/java/com/example/voicesofvalorant/ui/agents/AgentsFragment.java
+++ app/src/main/java/com/example/voicesofvalorant/ui/agents/AgentsFragment.java
@@ -23,13 +23,8 @@ public class AgentsFragment extends Fragment {
         agentsViewModel =
                 ViewModelProviders.of(this).get(AgentsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_agents, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        agentsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
         return root;
     }
 }
