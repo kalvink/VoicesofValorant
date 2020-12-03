312a9e0148d8d1619d23ff74851bcde2ea5ae78b
Kalvin <kalvin.kao@hotmail.com>
2020-12-02 23:01:10 -0500

    agent fragment update

--- app/src/main/java/com/example/voicesofvalorant/ui/agents/AgentsFragment.java
+++ app/src/main/java/com/example/voicesofvalorant/ui/agents/AgentsFragment.java
@@ -1,10 +1,12 @@
 package com.example.voicesofvalorant.ui.agents;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,7 +26,98 @@ public class AgentsFragment extends Fragment {
                 ViewModelProviders.of(this).get(AgentsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_agents, container, false);
 
+        View breach = root.findViewById(R.id.breachcv);
+        View brim = root.findViewById(R.id.brimcv);
+        View cypher = root.findViewById(R.id.cyphercv);
+        View jett = root.findViewById(R.id.jettcv);
+        View kill = root.findViewById(R.id.killcv);
+        View omen = root.findViewById(R.id.omencv);
+        View phoenix = root.findViewById(R.id.phoenixcv);
+        View raze = root.findViewById(R.id.razecv);
+        View reyna = root.findViewById(R.id.reynacv);
+        View sage = root.findViewById(R.id.sagecv);
+        View skye = root.findViewById(R.id.skyecv);
+        View sova = root.findViewById(R.id.sovacv);
+        View viper = root.findViewById(R.id.vipercv);
 
+        breach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        brim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        cypher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        jett.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        kill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        omen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        phoenix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        raze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        reyna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        sage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        skye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        sova.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
+        viper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("A", "test");
+            }
+        });
         return root;
     }
 }
