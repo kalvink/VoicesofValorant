241f182a12f46e8193d446e05e293fa365e8ef3a
Kalvin <kalvin.kao@hotmail.com>
2021-01-08 00:53:00 -0500

    clean breachsnds

--- app/src/main/java/com/example/voicesofvalorant/ui/agents/BreachSnds.java
+++ app/src/main/java/com/example/voicesofvalorant/ui/agents/BreachSnds.java
@@ -29,7 +29,6 @@ public class BreachSnds extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
-
         // Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -43,52 +42,65 @@ public class BreachSnds extends AppCompatActivity {
         return true;
     }
 
+    MediaPlayer mp;
+
     public void play1(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br1);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br1);
         mp.start();
     }
+
     public void play2(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br2);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br2);
         mp.start();
     }
+
     public void play3(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br3);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br3);
         mp.start();
     }
+
     public void play4(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br4);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br4);
         mp.start();
     }
+
     public void play5(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br5);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br5);
         mp.start();
     }
+
     public void play6(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br6);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br6);
         mp.start();
     }
+
     public void play7(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br7);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br7);
         mp.start();
     }
+
     public void play8(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br8);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br8);
         mp.start();
     }
+
     public void play9(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br9);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br9);
         mp.start();
     }
+
     public void play10(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br10);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br10);
         mp.start();
     }
+
     public void play11(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br11);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br11);
         mp.start();
     }
+
     public void play12(View view) {
-        MediaPlayer mp = MediaPlayer.create(BreachSnds.this, R.raw.br12);
+        mp = MediaPlayer.create(BreachSnds.this, R.raw.br12);
         mp.start();
     }
 }
